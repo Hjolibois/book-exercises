a05d9ad0e3aa7eaf4517ad110fc9752b4b984c36
hjolibois <hannahjolibois@gmail.com>
2019-02-01 18:43:22 -0800

    solved exercise 1

--- chapter-12-exercises/exercise-1/exercise.R
+++ chapter-12-exercises/exercise-1/exercise.R
@@ -1,33 +1,40 @@
 # Exercise 1: analyzing avocado sales with the `tidyr` package
 
 # Load necessary packages (`tidyr`, `dplyr`, and `ggplot2`)
-
+install.packages("tidyr")
+library(tidyr)
+library(ggplot2)
 
 # Set your working directory using the RStudio menu:
 # Session > Set Working Directory > To Source File Location
 
+
 # Load the `data/avocado.csv` file into a variable `avocados`
 # Make sure strings are *not* read in as factors
 
+avocados <- read.csv("data/avocado.csv", stringsAsFactors = F)
 
 # To tell R to treat the `Date` column as a date (not just a string)
 # Redefine that column as a date using the `as.Date()` function
 # (hint: use the `mutate` function)
-
+avocados <- avocados %>% mutate(as.Date(Date))
 
 # The file had some uninformative column names, so rename these columns:
 # `X4046` to `small_haas`
 # `X4225` to `large_haas`
 # `X4770` to `xlarge_haas`
 
+avocados <- avocados %>% rename(small_haas = X4046, large_haas = X4225, xlarge_haas = X4770)
 
 # The data only has sales for haas avocados. Create a new column `other_avos`
 # that is the Total.Volume minus all haas avocados (small, large, xlarge)
 
+avocados <- avocados %>% mutate(other_avos = Total.Volume - small_haas - large_haas - xlarge_haas)
 
 # To perform analysis by avocado size, create a dataframe `by_size` that has
 # only `Date`, `other_avos`, `small_haas`, `large_haas`, `xlarge_haas`
 
+by_size <- avocados %>% select(Date, other_avos, small_haas, large_haas, xlarge_haas)
 
 # In order to visualize this data, it needs to be reshaped. The four columns
 # `other_avos`, `small_haas`, `large_haas`, `xlarge_haas` need to be 
@@ -37,10 +44,12 @@
 # data frame to the `gather()` function. `size_gathered` will only have 3 
 # columns: `Date`, `size`, and `volume`.
 
+size <- by_size %>% gather(key = size, value = volume, -Date)
 
 # Using `size_gathered`, compute the average sales volume of each size 
 # (hint, first `group_by` size, then compute using `summarize`)
 
+avg_sale <- size %>% group_by(size)
 
 # This shape also facilitates the visualization of sales over time
 # (how to write this code is covered in Chapter 16)

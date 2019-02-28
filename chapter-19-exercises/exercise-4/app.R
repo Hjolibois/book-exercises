142dd3f3161d246d310953bc94ceb939db4cb1f7
hjolibois <hannahjolibois@gmail.com>
2019-02-28 08:44:09 -0800

    finished

--- chapter-19-exercises/exercise-4/app.R
+++ chapter-19-exercises/exercise-4/app.R
@@ -6,9 +6,13 @@ library("shiny")
 # Use source() to execute the `app_ui.R` and `app_server.R` files. These will
 # define the UI value and server function respectively.
 
+server <- source("app_server.R")
+
+ui <- source("app_ui.R")
 
 # You will need to fill in the `app_ui.R` file to create the layout.
 # Run the app through this file.
 
 # Create a new `shinyApp()` using the loaded `ui` and `server` variables
 
+shinyApp(ui = ui, server = server)
